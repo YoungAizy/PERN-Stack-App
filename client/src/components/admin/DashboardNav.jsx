fc19ca117cb46fe1224058de321ac487460f5df8
Ayanda Marotya <ayandamarotya@gmail.com>
2023-03-19 00:55:44 +0200

    add DashboardNav component

---
+++ client/src/components/admin/DashboardNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function DashboardNav(){
+
+    return(
+        <div>
+            <h2 className='d-inline-block'>My Reswtaurants</h2>
+            <button className='btn btn-outline-primary rounded-circle'><i className='fa fa-plus'></i></button>
+        </div>
+    )
+}

1cd7179278eee113423c67c1af15c6b40b02eaa6
youngAizy <ayandamarotya@gmail.com>
2023-11-18 02:12:31 +0200

    delete client app and backend folder

--- backend/Auth-Service/utils/constants/RequestType.js
+++
@@ -1,8 +0,0 @@
-export default {
-    REGISTRAION: "new_user",
-    LOGIN: "login",
-    UPDATE: "update_user",
-    updateEmail: "update_email",
-    updateName: "update_name",
-    updatePassword: "update_password"
-}

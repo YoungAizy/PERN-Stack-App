73cd57bc052a24fb68dffd8fdb15ae667737b6ae
youngAizy <ayandamarotya@gmail.com>
2023-11-18 18:42:47 +0200

    add reviews controller

---
+++ backend/RestaurantsService/controllers/reviewsController.js
@@ -0,0 +1,4 @@
+import Repop from "../Repository/reviews_repo";
+
+export const add_review =(req,res)=>{}
+export const del_review = (req,res)=>{}
--- backend/RestaurantsService/routes/protectedRoutes.js
+++ backend/RestaurantsService/routes/protectedRoutes.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import { addRestaurant, update, del_Restaurant } from '../controllers/restaurantController.js';
+import { add_review, del_review } from '../controllers/reviewsController.js';
 const restaurant_Router = express.Router();
+const reviewsRouter = express.Router();
 
+//RESTAURANT ROUTES
 restaurant_Router.post("/add", addRestaurant);
 restaurant_Router.put("/:id", update);
 restaurant_Router.delete("/:id", del_Restaurant);
+
+/** REVIEWS ROUTES */
+reviewsRouter.post("/publish", add_review);
+reviewsRouter.delete("/delete", del_review);
 
 export default restaurant_Router;
 

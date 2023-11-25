a71e3a8a1daca361c770507ef2af5e1d966aa252
youngAizy <ayandamarotya@gmail.com>
2023-11-25 05:22:55 +0200

    post new reviews under '/add' and new restaurants under '/publish'

--- backend/RestaurantsService/routes/protected.js
+++ backend/RestaurantsService/routes/protected.js
@@ -1,18 +1,19 @@
 import express from 'express';
-import { addRestaurant, update, del_Restaurant, upVote, downVote } from '../controllers/restaurantController.js';
+import { addRestaurant, update, del_Restaurant, upVote, downVote, getListings } from '../controllers/restaurantController.js';
 import { new_review, del_review, new_like, dislike, unlike, remove_disLike } from '../controllers/reviewsController.js';
 export const restaurant_Router = express.Router();
 export const reviewsRouter = express.Router();
 
 //RESTAURANT ROUTES
-restaurant_Router.post("/add", addRestaurant);
+restaurant_Router.post("/publish", addRestaurant);
 restaurant_Router.put("/update/:id", update);
 restaurant_Router.patch("/upvote/:id",upVote);
 restaurant_Router.patch("/downvote/:id",downVote);
+restaurant_Router.get("/myrestaurants", getListings);
 restaurant_Router.delete("/del/:id", del_Restaurant);
 
 /** REVIEWS ROUTES */
-reviewsRouter.post("/publish", new_review);
+reviewsRouter.post("/add", new_review);
 reviewsRouter.patch("/like/:id", new_like);
 reviewsRouter.patch("/unlike/:id", unlike);
 reviewsRouter.patch("/dislike/:id", dislike);

599c7bf9a4cec53cfa315117b9b4139c7de5a772
youngAizy <ayandamarotya@gmail.com>
2023-11-28 21:20:20 +0200

    store query statements in a single file

--- backend/Auth-Service/config/statements.js
+++ backend/Auth-Service/config/statements.js
@@ -1,17 +1,18 @@
 export default {
-    FETCH_EMAIL:{
+    FETCH_EMAIL_PREPARED:{
         name: "fetch_Email",
         text: "SELECT email FROM users WHERE email=$1",
         values: []
     },
-    UPDATE_EMAIL: "",
-    FETCH_PASSWORD: "SELECT password FROM users WHERE id=$1",
-    UPDATE_PASSWORD: "",
-    INSERT_USER: "INSERT INTO user(name,email,password) valueS($1,$2,$3) RETURNING *",
-    UPDATE_USER:"",
-    UPDATE_NAME: "",
-    FETCH_USER:"SELECT name,email FROM user WHERE id=$1",
-    FETCH_NAME: "SELECT username FROM users WHERE name=$1",
+    UPDATE_EMAIL: "UPDATE users SET email=$1 WHERE email=$2 returning email",
+    FETCH_PASSWORD: "SELECT password FROM users WHERE email=$1",
+    UPDATE_PASSWORD: "UPDATE users SET password = $1 WHERE email=$2",
+    INSERT_USER: "INSERT INTO users(full_name,email,password,userId) valueS($1,$2,$3,$4) RETURNING *",
+    UPDATE_USER:"UPDATE users SET full_name=$1, email=$2 WHERE email=$3 returning full_name, email",
+    UPDATE_NAME: "UPDATE users SET full_name =$1 WHERE email=$2 returning full_name",
+    FETCH_NAME:"SELECT full_name FROM user WHERE email=$1 returning name",
+    FETCH_LOGIN_CREDENTIALS: "SELECT email,password, userId FROM users WHERE email=$1",
+    DELETE_USER: "DELETE FROM users WHERE email=$1"
 
 }
 

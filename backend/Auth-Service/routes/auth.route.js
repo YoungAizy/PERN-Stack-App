584cf9f3ac9afebcec1915a926939b044a54aec3
youngAizy <ayandamarotya@gmail.com>
2023-11-28 21:44:22 +0200

    add middleware to routes

--- backend/Auth-Service/routes/auth.route.js
+++ backend/Auth-Service/routes/auth.route.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { register, signIn, deleteUser } from "../controllers/AuthController.js";
-// import credentialChecker from '../middleware/checkCredentials.js';
+import { register, signIn, deleteUser, update, getName } from "../controllers/AuthController.js";
+import checkPassword from "../middleware/checkPassword.js";
+import checkCredentials from "../middleware/checkCredentials.js";
+import verifyToken from "../middleware/verifyToken.js";
 const AuthRouter = express.Router();
 
-AuthRouter.post('/registration',register);
-AuthRouter.put('/update');
-AuthRouter.post('/login',signIn);
+AuthRouter.post('/registration',checkCredentials,register);
+AuthRouter.post('/login',checkCredentials,signIn);
+AuthRouter.patch('/update',verifyToken, checkPassword,update);
+AuthRouter.get('/name',verifyToken,getName);
 // AuthRouter.post('/logout',signOut);
-AuthRouter.delete('/:email',deleteUser);
+AuthRouter.delete('/delete',verifyToken,checkPassword,deleteUser);
 
 
 export default AuthRouter;

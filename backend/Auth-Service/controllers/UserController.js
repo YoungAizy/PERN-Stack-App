d16897e9ebbd3f29646de072ec025af67f5ef880
youngAizy <ayandamarotya@gmail.com>
2023-11-28 07:06:49 +0200

    remove profile files from Auth Service

--- backend/Auth-Service/controllers/UserController.js
+++
@@ -1,7 +0,0 @@
-//controller for profile routes
-
-
-export const createProfile =(req,res)=>{}
-export const updateProfile = (req,res)=>{}
-export const removeProfile =(req,res)=>{}
-export const retrieveProfile = (req,res)=>{}

435af4df1e43cafe7fda1b03de89d877fd0c11a5
youngAizy <ayandamarotya@gmail.com>
2023-11-28 17:28:00 +0200

    implement middleware for signup and login routes

--- backend/Auth-Service/middleware/checkCredentials.js
+++ backend/Auth-Service/middleware/checkCredentials.js
@@ -1,27 +1,44 @@
-import User from "../models/User.model";
-import UserRepo from "../repository/user_repo";
-
+import User from "../models/User.model.js";
+import UserRepo from "../repository/user_repo.js";
+import RequestType from "../utils/constants/RequestType.js";
+import { v4 as uuidv4 } from 'uuid';
 
 export default async (req, res, next) => {
-    console.log("middleware entered!");
-    let emailPass = false;
+
     const _request = req.body.request_type;
-    console.log(req.body);
-    const data = { full_name: req.body.data.name, email:escape(req.body.data.email).toLowerCase(), 
-        password:req.body.data.password, username: req.body.data.username}
+    const {full_name, email,password} = req.body.data;
+    const data = { full_name, email:email.toLowerCase(), password:password}
+
     const user = new User(data);
     const repo = new UserRepo(user);
 
-    const email = await repo.retrieve(_request);
-    if (email.rowCount >= 1) {
-        console.log(email.rows);
-        res.send("Email already exist in our database. Log in instead.");
+    let _email, _password, userId;
+    if (_request === RequestType.LOGIN) {
+        //get email and password from db
+        const result = await repo.retrieveCredentials(RequestType.LOGIN);
+       
+        if (!result) {
+            //TODO: res.send() a message saying user doesn't exist on database
+            res.send({message:"Email not registered"})
+            return;
+        }
+        _email= result.email;
+        _password = result.password;
+        userId =result.userid;
+        req.body.credentials = {hashedPassword: _password, userId};
+        next();
+        return
+    }
     
-    } else
-        emailPass = true;
-   
-    req.body.passedTest = emailPass;
-    req.user = data;
+    _email = await repo.retrieveCredentials(_request);
+
+    if (_email) {
+        res.send("Email already exist in our database. Log in instead.");
+        return;
+    }
+    
+    data.userId = userId = uuidv4();
+    req.body.user = data;
     next();  
 }
 

90a380742a49f298fc6af4fabf023472379f8006
youngAizy <ayandamarotya@gmail.com>
2023-11-29 10:53:16 +0200

    update statements file

--- backend/Auth-Service/controllers/AuthController.js
+++ backend/Auth-Service/controllers/AuthController.js
@@ -1,29 +1,34 @@
 import bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken";
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import jose from 'node-jose'
 import User from '../models/User.model.js';
 import UserRepo from '../repository/user_repo.js';
 import RequestType from '../utils/constants/RequestType.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-export const register = async(req,res,next)=>{
-    //don't process anything if e-mail or username already exist in database.
-    if(!req.body.passedTest) next();
+//Read signing key from pem file
+const privateKey = fs.readFileSync(path.resolve(__dirname,"../certs/private-key.pem"));
+
+
+export const register = async(req,res)=>{
+    console.log("Registration controller");
+    const repo = req.body.repo;
+    const data = req.body.user;
+    console.log(data)
 
     try {
-        const data = req.user;
-        data.hashedPassword = await bcrypt.hash(req.body.password, 8);
-
-        const user = new User(data);
-        const repo = new UserRepo(user);
         const result = await repo.insert();
-    
+
         //create a token and send it back to client in the http header
         if (Object.keys(result).length >= 4) {
-            const user = {name:result['full_name'],email:result.email};
-            const accessToken = jwt.sign(user, "Hello there mister peabody");
-            res.json({accessToken: accessToken,user: user});
+            const user = {userId:result.userid,email:result.email};
+            const accessToken = jwt.sign(user, privateKey,{algorithm:"RS256"});
+            res.json({accessToken: accessToken});
         }else{
             console.log(err);
             res.send("Could not register user. Error uploading info");
@@ -35,84 +40,79 @@ export const register = async(req,res,next)=>{
 }
 
 export const signIn = (req,res)=>{
-    const privateKey = fs.readFileSync("./certs/private");
-    // const user = new User(req.body.data,RequestType.LOGIN);
-    // const repo = new UserRepo(user);
-    const user = {
-        name: "Ayanda M",
-        email: 'ayandam@hotmail.com'
-    }
-    let key;
-
+    const {hashedPassword,userId} = req.body.credentials;
+    
     try {
-        jose.JWK.asKey(privateKey,"pem").
-        then(function(result) {
-          key = result;
-          
-        });
-        // const accessToken = jwt.sign(user,privateKey);
-        // res.send(accessToken);
-        // if (bcrypt.compareSync(req.body.password, req.hashedPassword)) {
-        //     const user = {
-        //         name: req.name,
-        //         email: req.body.email
-        //     }
-        //     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-        //     const encryptedJWT = jose.EncryptJWT();
-        //     res.json({
-        //         accessToken: accessToken,
-        //         user: req.user
-        //     });
-        // } else
-        //     res.send("Password Authentication Failed")
+        if (bcrypt.compareSync(req.body.data.password, hashedPassword)) {
+            const token_data = {
+                email: req.body.data.email,
+                userId //use uuidv4 to create userId durimg signup
+            }
+            const accessToken = jwt.sign(token_data, privateKey, {algorithm: "RS256"});
+            //const encryptedJWT = jose.EncryptJWT();
+            res.send({accessToken});
+        } else
+            res.send({message:"Incorrect password given."})
     } catch (error) {
         console.log(error);
-        res.send("Login Failed");
+        res.send({message:"Login Failed"});
     }
 
 }
 
-export const getUserInfo = async(req,res)=>{
-    //user sends token to server to get details
-    const user = new User(req.body.data,RequestType.UPDATE);
-    const repo = new UserRepo(user);
-    const result = await repo.retrieve(RequestType.UPDATE);
-    res.json({data:result})
+export const getName = async(req,res)=>{
+    const {email} = req.body;
+
+    const repo = new UserRepo(new User(email,RequestType.GET_NAME));
+    try {
+        const result = await repo.name;
+        res.send(result);
+    } catch (error) {
+        console.log(error)
+    }
 
 }
 
-export const update = async(req,res,next)=>{
-    const user = new User()
-    const repo = new UserRepo(user)
+export const update = async(req,res)=>{
+    const {repo }= req.body;
     let result;
-
+console.log("Update Controller")
     const {request_type} = req.body;
     try {
         switch (request_type) {
             case RequestType.updateName:
-                result = repo.update(RequestType.updateName)
+                result = await repo.update(RequestType.updateName);
                 break;
             case RequestType.updateEmail:
-                result = repo.update(RequestType.updateEmail)
+                result = await repo.update(RequestType.updateEmail);
                 break;
             case RequestType.updatePassword:
-                const newPassword = req.body.password;
-                passwordUpdate(newPassword);
+                const {password,new_password} = req.body.data;
+
+                if(password !== new_password){
+                    result = await repo.update(request_type);
+                }
                 break;
             default:
+                console.log("default...")
                 //default behavior is updating both name and email
-                result = repo.update(RequestType.UPDATE)
+                result = await repo.update(RequestType.UPDATE)
                 break;
         }
-        res.json({data:result})
+        res.json({data:result})//TODO: check result.rowCount == 1
     } catch (error) {
         
     }
 }
 
 // export const signOut = (req,res)=>{}
-export const deleteUser = (req,res)=>{}
-
-const passwordUpdate = (newPassword,oldPassword)=>{
-
+export const deleteUser = async (req,res)=>{
+    const {repo} = req.body;
+    
+    try {
+        const results = await repo.delete();
+        if(results.rowCount === 1) res.send("User Deleted");
+    } catch (error) {
+        console.log(error);
+    }
 }

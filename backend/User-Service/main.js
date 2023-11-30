acd490139fa950da69825cda7430c7bdf614c00a
youngAizy <ayandamarotya@gmail.com>
2023-11-30 13:17:50 +0200

    add test route

--- backend/User-Service/main.js
+++ backend/User-Service/main.js
@@ -28,5 +28,9 @@ app.use(hpp());
 
 app.use('/api/v1/profile', profileRouter);
 
+app.get('/test',(req,res)=>{
+    res.send("API WORKING!")
+});
+
 const port = process.env.PORT || 6001
 app.listen(port, () => console.log(`Server is up and running on port ${port}`));

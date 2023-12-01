93080f0c2b8460a9b151c4f6e241f1a6fc325f68
youngAizy <ayandamarotya@gmail.com>
2023-12-01 15:48:34 +0200

    rename reviews api call file

--- client/src/apis/review.js
+++
@@ -1,9 +0,0 @@
-import databinder from './databinder';
-
-export default {
-    post: databinder.post('/reviews/add'),
-    like(id){ databinder.patch(`/reviews/like/${id}`)},
-    dislike(id){ databinder.patch(`/reviews/dislike/${id}`)},
-    get(id){ databinder.get(`/reviews?createdby=${id}`)},
-    delete(id){databinder.delete(`/reviews/delete/${id}`)}
-}

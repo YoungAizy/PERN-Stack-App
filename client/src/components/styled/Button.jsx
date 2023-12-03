00fa622c7d58ebc743caff428792d9b0308f5d26
youngAizy <ayandamarotya@gmail.com>
2023-12-03 22:55:16 +0200

    style primary button for app

--- client/src/components/styled/Button.jsx
+++ client/src/components/styled/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({onBtnClick, text, btnType, bg, placement, disabled = false}) => {
   return (
-    <div>Button</div>
+    <div className={`form-margin ${placement}`}>
+      <button type={btnType} onClick={onBtnClick} disabled={disabled}
+      className={`btn ${disabled ? "btn-secondary":"bg-primary"}`} 
+      style={{color:"white", ...bg}}>
+      {text}
+      </button>
+    </div>
   )
 }
 

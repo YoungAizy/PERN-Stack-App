6c7fb0fa89172f9deafd8c63ad7a298a1818ee5a
youngAizy <ayandamarotya@gmail.com>
2023-12-03 23:00:43 +0200

    update double switch styling

--- client/src/components/DoubleSwitch.jsx
+++ client/src/components/DoubleSwitch.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import './componets.css';
+import '../styling/doubleSwitch.css';
 
-const DoubleSwitch = ({LeftTag, RightTag}) => {
+const DoubleSwitch = ({LeftTag, RightTag, leftClick, rightClick}) => {
 
     const moveSlider = (direction)=>{
       const slider = document.getElementById('slider-bg')
       if(direction === 'left'){
-        slider.style.left = 0;
+        slider.style.left = '0';
+        leftClick(true);
       }else{
-        slider.style.right = 0;
+        slider.style.left = '120px';
+        rightClick(false)
       }
     }
 
   return (
-    <div className='switch-container rounded-pill p-2 border border-primary'>
-        <div className="switch-btns">
-            <div id='slider-bg' className="switch-slider rounded-pill"></div>
-            <button id='reviewer-switch' className="switch-btn transparent-bg" onClick={moveSlider}>{LeftTag}</button>
-            <button id='restaurateur-switch' className="switch-btn transparent-bg" onClick={moveSlider}>{RightTag}</button>
+    <div className='switch-container'>
+        <div className="switch-btns rounded-pill">
+            <div id='slider-bg' className="rounded-pill"></div>
+            <button id='reviewer-switch' type='button' className="switch-btn" onClick={()=>moveSlider("left")}>{LeftTag}</button>
+            <button id='restaurateur-switch' type='button' className="switch-btn" onClick={()=>moveSlider("right")}>{RightTag}</button>
         </div>
     </div>
   )

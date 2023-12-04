327c2612b1da00c0b8307151474f84213d05a358
youngAizy <ayandamarotya@gmail.com>
2023-12-04 09:24:34 +0200

    update reviews component

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -3,16 +3,16 @@ import StarRating from './StarRating'
 
 function Reviews({ reviews }) {
     return (
-        <div style={{ margin: "1rem" }} className="row row-cols-3 mb-2">
+        <div className="row row-cols-3 mb-2 m-3">
             {reviews && reviews.map((review) => {
                 return (
                     <div key={review.id} className="card text-white bg-primary mb-3" style={{ maxWidth: "30%", margin: "8px" }}>
                         <div className="card-header d-flex justify-content-between">
-                            <span>{review.name}</span>
+                            <span>{review.username}</span>
                             <span><StarRating rating={review.rating} /></span>
                         </div>
                         <div className="card-body">
-                            <p className="card-text">{review.review}</p>
+                            <p className="card-text">{review.review_text}</p>
                         </div>
                     </div>
                 )

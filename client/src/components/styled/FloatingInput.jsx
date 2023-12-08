6c6db63c31be0792c73fa53869b891d1b3a4c4fb
youngAizy <ayandamarotya@gmail.com>
2023-12-08 20:02:27 +0200

    add required attribute to styled inputs

--- client/src/components/styled/FloatingInput.jsx
+++ client/src/components/styled/FloatingInput.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const FloatingInputField = ({inputId, value, inputType, label, placeholder, onInputChanged})=>{
+const FloatingInputField = ({inputId, value, inputType, label, placeholder, onInputChanged, isRequired = true})=>{
 
 
     return(
         <div className="form-floating form-margin">
-            <input id={inputId} value={value} type={inputType} className="form-control" placeholder={placeholder} onChange={e=>onInputChanged(e.target.value)} />
+            <input id={inputId} value={value} type={inputType} className="form-control" placeholder={placeholder} onChange={e=>onInputChanged(e.target.value)} required={isRequired} />
             <label htmlFor={inputId}>{label}</label>
         </div>
     )
--- client/src/components/styled/TextInput.jsx
+++ client/src/components/styled/TextInput.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
-export const TextInput = ({inputId, label, val, inputType, fieldName, placeholder = "", onChangeEvent}) => {
+export const TextInput = ({inputId, label, val, inputType, fieldName, placeholder = "", onChangeEvent, isRequired= true}) => {
   return (
     <div className="form-margin col-5">
     <label htmlFor={inputId} >{label}</label>
-    <input value={val} type={inputType} name={fieldName} id={inputId} className="form-control" placeholder={placeholder} onChange={e=>onChangeEvent(e.target.value)}/>
+    <input value={val} type={inputType} name={fieldName} id={inputId} className="form-control" placeholder={placeholder} onChange={e=>onChangeEvent(e.target.value)} required={isRequired}/>
 </div>
   )
 }
 
-export const HintedLabelInput = ({inputId, label, val, inputType, fieldName, onChangeEvent})=>{
+export const HintedLabelInput = ({inputId, label, val, inputType, fieldName, onChangeEvent, isRequired})=>{
 
     return(
         <div className="form-margin form-floating">
-            <input value={val} type={inputType} name={fieldName} id={inputId} className="form-control" onChange={onChangeEvent}/>
+            <input value={val} type={inputType} name={fieldName} id={inputId} className="form-control" onChange={onChangeEvent} required={isRequired} />
             <label htmlFor={inputId} >{label}</label>
         </div>
     )

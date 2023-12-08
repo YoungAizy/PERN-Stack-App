c300504935c6f515612f371d9fab1b0d829bb9c7
youngAizy <ayandamarotya@gmail.com>
2023-12-08 19:54:09 +0200

    create request type definitions file

---
+++ client/src/utils/requestTypes.js
@@ -0,0 +1,30 @@
+export const userRequests = {
+    REGISTRATION: "new_user",
+    VERIFICATION: "verify",
+    LOGIN: "login",
+    UPDATE: "update_user",
+    insertUserId: "update_uuid",
+    updateEmail: "update_email",
+    updateEmailVerification: "update_email_verify",
+    updateName: "update_name",
+    updatePassword: "update_password",
+    passwordReset: "account_recovery",
+    confirmPasswordReset: "confirm_account_recovery",
+    LOGOUT: "logout",
+    DELETE: "delete",
+}
+
+export const profileRequests = {
+    CREATE: "new_profile",
+    UPDATE: "update_profile",
+    GET: "fetch_profile",
+    DELETE: "delete_profile"
+}
+
+export const reviewsRequests = {
+
+}
+
+export const restaurantsRequests = {
+
+}

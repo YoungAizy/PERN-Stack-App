928e1e5ea96e6ce43102d69ba21daf53fc11d125
youngAizy <ayandamarotya@gmail.com>
2023-12-09 12:06:04 +0200

    create profile api calling functions

--- client/src/apis/profile.js
+++ client/src/apis/profile.js
@@ -8,7 +8,23 @@ const profileApi= {
     async create(payload) {
         const result = await databinder.post('/create',payload);
         return result;
-    }
+    },
+    async fetch(){
+        const result = await databinder.get('/fetch');
+        return result;
+    },//get request
+    async update(payload){
+        const result = await databinder.put('/update',payload);
+        return result;
+    },//put request
+    async updateImage(payload){
+        const result = databinder.put('/update/image', payload);
+        return result;
+    },
+    async delete(userId){
+        const result = await databinder.delete(`/${userId}`);
+        return result;
+    }//delete request
 }
 
 export default profileApi;

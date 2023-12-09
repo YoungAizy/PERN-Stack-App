803a56e5b7adf1dc18a299bcb9039102bcd5b008
youngAizy <ayandamarotya@gmail.com>
2023-12-09 11:37:19 +0200

    add colum props to DOB component

--- client/src/components/DOB.jsx
+++ client/src/components/DOB.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
-const DOB = () => {
+const DOB = ({dayCol = "col-2", monthCol = "col-2", yearCol= "col-2"}) => {
   return (
     <div className='m-1 row'>
         <p>Birthday:</p>
-        <div className='col-2'>
+        <div className={dayCol}>
             <select className='form-select' name="dob" id="day">
                 <option value="25">25</option>
             </select>
         </div>
-        <div className='col-2'>
+        <div className={monthCol}>
             <select className='form-select' name="dob" id="month">
                 <option value="may">December</option>
             </select>
         </div>
-        <div className='col-2'>
+        <div className={yearCol}>
             <select className='form-select' name="dob" id="year">
                 <option value="1997">1989</option>
             </select>

ed05b7ddee0c1c46de149fc4833503b43c96521d
youngAizy <ayandamarotya@gmail.com>
2023-12-09 11:13:40 +0200

    signin user after successful registration

---
+++ utils/redirectToLogin.js
@@ -0,0 +1,10 @@
+const {unverifiedUsers} = require('../memory/signup_dictionary.js');
+const { SignIn } = require('../services/signin.service.js');
+
+exports.redirectToLoginService = async (email)=>{
+    //first lookup password in signup dictionary;
+    const userPassword = unverifiedUsers[email];
+    const tokens = await SignIn(email,userPassword);
+    tokens && delete unverifiedUsers[email];
+    return tokens;
+}

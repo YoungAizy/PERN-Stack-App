32410b5a2f5342b9d226081eb2daf30196977fe2
youngAizy <ayandamarotya@gmail.com>
2023-12-09 11:31:58 +0200

    create gender options styling file

--- client/src/components/GenderOptions.jsx
+++ client/src/components/GenderOptions.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import '../styling/genderOptions.css';
 
-const GenderOptions = () => {
+const GenderOptions = ({onGenderChange:setGender}) => {
   return (
     <div className='form-margin gender-group'>
       <p>Gender:</p>
       <div className="form-check form-check-inline">
         <label className='form-check-label' htmlFor="male">Male</label>
-        <input className='form-check-input' type="radio"  value="M" name="gender" id="male" />
+        <input className='form-check-input' type="radio"  value="M" name="gender" id="male" onChange={e=>setGender(e.target.value)} />
       </div>
       <div className="form-check form-check-inline" >
         <label className='form-check-label' htmlFor="female">Female</label>
-        <input className='form-check-input' type="radio"  value="F" name="gender" id="female" />
+        <input className='form-check-input' type="radio"  value="F" name="gender" id="female" onChange={e=>setGender(e.target.value)} />
       </div>
       <div className="form-check form-check-inline" >
         <label className='form-check-label' htmlFor="other">Other</label>
-        <input className='form-check-input' type="radio"  value="O" name="gender" id="other" />
+        <input className='form-check-input' type="radio"  value="O" name="gender" id="other" onChange={e=>setGender(e.target.value)} />
       </div>
     </div>
   )

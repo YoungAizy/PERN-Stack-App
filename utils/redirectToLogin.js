16c54baec878d4e5cae59f66208049d090ae657a
youngAizy <ayandamarotya@gmail.com>
2023-12-10 03:31:01 +0200

    add console logs for debugging

--- utils/redirectToLogin.js
+++ utils/redirectToLogin.js
@@ -2,9 +2,13 @@ const {unverifiedUsers} = require('../memory/signup_dictionary.js');
 const { SignIn } = require('../services/signin.service.js');
 
 exports.redirectToLoginService = async (email)=>{
+    console.log("email:", email);
+    console.log("User emails:",unverifiedUsers)
+    console.log("User password:",unverifiedUsers[email])
     //first lookup password in signup dictionary;
     const userPassword = unverifiedUsers[email];
     const tokens = await SignIn(email,userPassword);
     tokens && delete unverifiedUsers[email];
+    tokens && console.log("after deletion:", unverifiedUsers)
     return tokens;
 }

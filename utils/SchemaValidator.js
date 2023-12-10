4e47682fd1137e905b065287efbb5df0947c2837
youngAizy <ayandamarotya@gmail.com>
2023-12-10 17:41:14 +0200

    update new profile schema validator with mins

--- utils/SchemaValidator.js
+++ utils/SchemaValidator.js
@@ -1,17 +1,17 @@
 import Joi from 'joi';
 
 const profileSchema = Joi.object({
-    username: Joi.string().min(6).required(),
+    username: Joi.string().min(4).max(16).required(),
     sex: Joi.string().min(1).max(1).required(),
-    company: Joi.string().max(100),
-    position: Joi.string().max(20).optional(),
+    company: Joi.string().min(0).max(100).optional(),
+    position: Joi.string().min(0).max(20).optional(),
     account_type: Joi.string().max(13).required(),
     city: Joi.string().required(),
-    dob: Joi.string().required()
+    dob: Joi.string().min(10).max(10).required()
 })
 
 const profileUpdateSchema = Joi.object({
-    username: Joi.string().min(6),
+    username: Joi.string().min(4),
     sex: Joi.string().min(1).max(1),
     city: Joi.string(),
     dob: Joi.string(),

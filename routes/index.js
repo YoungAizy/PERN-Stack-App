50f65c88c5635c184889aa4cae97ad80085cf7b2
youngAizy <ayandamarotya@gmail.com>
2023-12-11 21:19:07 +0200

    add middleware to routes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import { fetchProfile, newProfile, updateProfile,deleteProfile, updateImage } from '../controllers/index.js';
 import multer from 'multer';
+import { getUserId } from '../middleware/getUserId.js';
+import transferToken from '../middleware/transferToken.js';
 
 const profileRouter = express.Router();
 
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage });
 
-profileRouter.post('/create', upload.single('avatar'),newProfile);
-profileRouter.get('/fetch',fetchProfile);
-profileRouter.put('/update', updateProfile);
-profileRouter.put('/update/image', upload.single('avatar'), updateImage);
-profileRouter.delete('/delete',deleteProfile);
+profileRouter.post('/create', upload.single('avatar'),newProfile, getUserId);
+profileRouter.get('/fetch',transferToken , getUserId ,fetchProfile);
+profileRouter.put('/update', getUserId, updateProfile);
+profileRouter.put('/update/image', upload.single('avatar'), updateImage, getUserId);
+profileRouter.delete('/delete', transferToken, getUserId,deleteProfile);
 
 export default profileRouter;

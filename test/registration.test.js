ff2cd01ba7d9df007f9cec5a97beeb89bcfc93fe
youngAizy <ayandamarotya@gmail.com>
2023-12-11 21:21:51 +0200

    demo test file

---
+++ test/registration.test.js
@@ -0,0 +1,11 @@
+import {expect} from "chai";
+
+// describe("Registration Flow",()=>{
+//     it("should accept an object as req and extract token",()=>{
+//         //Arrange 
+//         const accessToken = "eyJraWQiOiJuNXFYUWF3K04rbWN6Z3dyelg5c0Q0SmF0djFKZk5yaENJUzNZMnBNT1g4PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJmYzJkMGJiOS1jODdjLTRhNTUtOTM1OC1jMDMzY2FjZDRhMmEiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0yLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMl9haHMyVjNJN20iLCJjbGllbnRfaWQiOiIzb2piZ2o5MzllaTVmbDZhbnIxNW5lNmJ0aSIsIm9yaWdpbl9qdGkiOiJiNTE3MDk5Mi01NmNiLTQwM2UtODNmMC1kZmZhMzA2ZmZiZTciLCJldmVudF9pZCI6ImU4NmY4YTIxLWUwMjYtNDAzMC05OGVhLTg3OTgxNjk4OWE5YiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE3MDIyODkzNDAsImV4cCI6MTcwMjM3NTc0MCwiaWF0IjoxNzAyMjg5MzQwLCJqdGkiOiI4M2RjZmQ5MS03NGZlLTQ1MjAtYjBjNi04OTdkNmQ4MTVjMzUiLCJ1c2VybmFtZSI6ImZjMmQwYmI5LWM4N2MtNGE1NS05MzU4LWMwMzNjYWNkNGEyYSJ9.LKnJFL64FtjWcbIRiVwQpv5Y43K0qC36yFlBHHKdPx2oTaXBZKjuubjKAyu7iwjSDNsXfuelPvcweUO2ElNKH1xu1Z_DtyyUX4uKJ7aQMpaeI4Js19RMN2_KqV50vYqW7TxpPJf55G5Bz8uZrhZ1O7Q3JQ-WTXxcsy8XgXcE_MIXlz0cALo1uPJgYfQ2KXPwODEHC306_wa5kamNK-akeKIfPJe11t7MVt6W_k1EmxAwefT-eRWTj9ZkVatqx7w6Al9MRrsW9vgBG8YmItci74GtzLI6-Ktn_RGzAnRIsooSURAINq8yl8RDF86wlqo-fCaGYxVqC_Y33Mn0Yi9gMQ";
+
+//         //Act
+//         const result = 
+//     })
+// })

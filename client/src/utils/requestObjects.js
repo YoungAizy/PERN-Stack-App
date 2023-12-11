0597e072c53b35ea46a77e9393a8c291c58d245b
youngAizy <ayandamarotya@gmail.com>
2023-12-11 13:29:15 +0200

    add updateProfile schema

--- client/src/utils/requestObjects.js
+++ client/src/utils/requestObjects.js
@@ -1,7 +1,9 @@
 export const newUser = (firstname,surname,email,password)=> ({firstname,surname,email,password});
 
-export const profileSchema = (username,gender,d_o_b,city,user_role,companyName,companyPosition) => ({
-    username,gender,d_o_b,city,user_role,companyName,companyPosition});
+export const updateProfile = (username,city,d_o_b,gender)=> ({username,city,d_o_b,gender})
+
+export const profileSchema = (username,gender,d_o_b,city,
+    user_role,companyName,companyPosition) => ({username,gender,d_o_b,city,user_role,companyName,companyPosition});
 
 export const restaurantSchema = () => ({});
 

06a019ab5416d707ff6d28623465fca722c4ac0a
youngAizy <ayandamarotya@gmail.com>
2023-12-11 13:23:22 +0200

    update months file

--- client/src/utils/Months.js
+++ client/src/utils/Months.js
@@ -1,15 +1,21 @@
 /* eslint-disable import/no-anonymous-default-export */
-export default {
-    Jan: {id: "January", val: "Jan", days: 31},
-    Feb: {id: "February", val:"Feb",  days: 28},
-    Mar: {id: "March", val: "Mar",  days: 31},
-    Apr: {id: "April", val: "Apr", days: 30},
-    May: {id: "May", val: "May", days: 31},
-    Jun: {id: "June", val: "Jun", days: 30},
-    Jul: {id: "July", val: "Jul", days: 31},
-    Aug: {id: "August", val: "Aug", days: 31},
-    Sep: {id: "September", val: "Sep", days:30},
-    Oct: {id: "October", val: "Oct", days:31},
-    Nov: {id: "November", val: "Nov", days:30},
-    Dec: {id: "December", val: "Dec", days: 31},
+export const days = {
+    feb: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28],
+    thirty() { return [...this.feb, 29,30]},
+    thirtyOne() {return [...this.thirty(),31]}
 }
+
+export default [
+    {month: "Jan", val: "01", days: days.thirtyOne()},
+    {month: "Feb", val: "02",  days: days.feb},
+    {month: "Mar", val: "03",  days: days.thirtyOne()},
+    {month: "Apr", val: "04", days: days.thirty()},
+    {month: "May", val: "05", days: days.thirtyOne()},
+    {month: "June", val: "06", days: days.thirty()},
+    {month: "July", val: "07", days: days.thirtyOne()},
+    {month: "Aug", val: "08", days: days.thirtyOne()},
+    {month: "Sep", val: "09", days: days.thirty()},
+    {month: "Oct", val: "10", days: days.thirtyOne()},
+    {month: "Nov", val: "11", days: days.thirty()},
+    {month: "Dec", val: "12", days: days.thirtyOne()},
+]

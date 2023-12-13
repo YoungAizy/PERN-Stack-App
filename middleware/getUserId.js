346defbcb1754f4a7d798bf91ddef518d9ef2726
youngAizy <ayandamarotya@gmail.com>
2023-12-13 09:20:26 +0200

    update getUserid middleware

--- middleware/getUserId.js
+++ middleware/getUserId.js
@@ -1,4 +1,6 @@
-import fetch from 'node-fetch';
+require('dotenv').config()
+import jwt from 'jsonwebtoken';
+import jwksClient from 'jwks-rsa';
 import requestType from '../utils/requestType.js';
 
 
@@ -6,29 +8,37 @@ export const getUserId = async(req,res,next)=>{
     // abort function if request is not from the registration process
     // 
 
-    const {accessToken} = req.body;
-    console.log("kkk",accessToken);
-    const requestBody = {
-        request_type: "get_user",
-        data:{
-            "custom:userid": "ok"
-        }
+    const token = req.cookies.idToken;
+
+    let userId;
+    if (token != null) {
+        const decoded = jwt.decode(token, {complete: true});
+        
+        const client = jwksClient({
+            jwksUri: `https://cognito-idp.${process.env.AWS_REGION}.amazonaws.com/${process.env.COGNITO_USER_POOL_ID}/.well-known/jwks.json`,
+            cache: true,
+            rateLimit: true,
+        });
+
+        const kid = decoded.header.kid;
+        const key = await client.getSigningKey(kid);
+        const signingKey = key.getPublicKey();
+    
+        jwt.verify(token, signingKey, { algorithms: ['RS256'] }, (err,data)=>{
+            if(err) {
+                console.log("Error occured", err);
+                res.status(403).json({message: err.message});
+                return;
+            };
+    
+            userId = data['custom:userid'];
+        })
+    }else{
+        res.status(400).json({message: "Missing Access Token"});
+        return;
     }
-    const response = await fetch('http://localhost:7009/api/v1/auth/user', {
-        method: 'post',
-        body: JSON.stringify(requestBody),
-        headers: {'Content-Type': 'application/json', 'Authorization': `Bearer ${accessToken}` },
-
-    });
-    // console.log("Response from Cognito", response);
-    const data = await response.json();
-
-    console.log("data",data);
-    // const result = data.filter(attr => attr.Name === "custom:userid");
-    console.log("result:", data[0].Value);
-    req.body.userid =  data[0].Value;
     
     if((req.body.request === requestType.CREATE)) return data[0].Value;
-    
+    req.body.userid = userId;
     next();
 }

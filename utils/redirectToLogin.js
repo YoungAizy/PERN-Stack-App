a0933b1b2607cf65702831210b7f9cd35ce4181d
youngAizy <ayandamarotya@gmail.com>
2023-12-13 09:34:58 +0200

    update signup redirect

--- utils/redirectToLogin.js
+++ utils/redirectToLogin.js
@@ -1,14 +1,12 @@
 const {unverifiedUsers} = require('../memory/signup_dictionary.js');
-const { SignIn } = require('../services/signin.service.js');
+const { Authenticate} = require('../services/auth.service.js');
 
 exports.redirectToLoginService = async (email)=>{
-    console.log("email:", email);
-    console.log("User emails:",unverifiedUsers)
-    console.log("User password:",unverifiedUsers[email])
     //first lookup password in signup dictionary;
     const userPassword = unverifiedUsers[email];
-    const tokens = await SignIn(email,userPassword);
+
+    const tokens = await Authenticate("USER_PASSWORD_AUTH", {"USERNAME":email,"PASSWORD":userPassword});
     tokens && delete unverifiedUsers[email];
-    tokens && console.log("after deletion:", unverifiedUsers)
+
     return tokens;
 }

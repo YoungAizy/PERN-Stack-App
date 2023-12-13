27407204d4c13d8b151f8c8324336276ab17e78e
youngAizy <ayandamarotya@gmail.com>
2023-12-14 00:29:59 +0200

    configure cors origin whitelist

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ import 'dotenv/config'
 
 const app = express();
 
-app.use(cors())
+app.use(cors({origin:['http://localhost:3000'], credentials: true}))
 app.use(helmet())
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: false}));
 app.use(hpp());
 app.use(cookieParser())
 app.use(morgan('dev'))

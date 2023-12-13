e351eac47608e937c5d27adbcc54af45dd67e452
youngAizy <ayandamarotya@gmail.com>
2023-12-13 09:32:07 +0200

    revoke token instead of global signout

--- services/signOut.service.js
+++ services/signOut.service.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const { CognitoIdentity } = require("../config/cognito.js");
 
 exports.signOut = async (token)=>{
     const params = {
-        AccessToken: token
+        ClientId: process.env.COGNITO_POOL_CLIENT_ID,
+        Token: token
     }
 
-    const result = await CognitoIdentity.globalSignOut(params).promise();
+    const result = await CognitoIdentity.revokeToken(params).promise();
     return result;
 }

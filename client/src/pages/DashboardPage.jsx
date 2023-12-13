be3bba791f4a279c5e8b05416803e42aca13c88e
youngAizy <ayandamarotya@gmail.com>
2023-12-13 23:43:55 +0200

    correctly dispatch saveProfile action

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -30,6 +30,8 @@ export default function DashboardPage(){
     const profile = useSelector(state=> state.profile.profile);
     const dispatch = useDispatch();
 
+    console.log("PROFILE:", profile);
+
 
     useEffect(() => {
         const screen = window.innerWidth;
@@ -38,16 +40,16 @@ export default function DashboardPage(){
         }
     }, []);
 
-    const token = "eyJraWQiOiJuNXFYUWF3K04rbWN6Z3dyelg5c0Q0SmF0djFKZk5yaENJUzNZMnBNT1g4PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJmYzJkMGJiOS1jODdjLTRhNTUtOTM1OC1jMDMzY2FjZDRhMmEiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0yLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMl9haHMyVjNJN20iLCJjbGllbnRfaWQiOiIzb2piZ2o5MzllaTVmbDZhbnIxNW5lNmJ0aSIsIm9yaWdpbl9qdGkiOiJiNTE3MDk5Mi01NmNiLTQwM2UtODNmMC1kZmZhMzA2ZmZiZTciLCJldmVudF9pZCI6ImU4NmY4YTIxLWUwMjYtNDAzMC05OGVhLTg3OTgxNjk4OWE5YiIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE3MDIyODkzNDAsImV4cCI6MTcwMjM3NTc0MCwiaWF0IjoxNzAyMjg5MzQwLCJqdGkiOiI4M2RjZmQ5MS03NGZlLTQ1MjAtYjBjNi04OTdkNmQ4MTVjMzUiLCJ1c2VybmFtZSI6ImZjMmQwYmI5LWM4N2MtNGE1NS05MzU4LWMwMzNjYWNkNGEyYSJ9.LKnJFL64FtjWcbIRiVwQpv5Y43K0qC36yFlBHHKdPx2oTaXBZKjuubjKAyu7iwjSDNsXfuelPvcweUO2ElNKH1xu1Z_DtyyUX4uKJ7aQMpaeI4Js19RMN2_KqV50vYqW7TxpPJf55G5Bz8uZrhZ1O7Q3JQ-WTXxcsy8XgXcE_MIXlz0cALo1uPJgYfQ2KXPwODEHC306_wa5kamNK-akeKIfPJe11t7MVt6W_k1EmxAwefT-eRWTj9ZkVatqx7w6Al9MRrsW9vgBG8YmItci74GtzLI6-Ktn_RGzAnRIsooSURAINq8yl8RDF86wlqo-fCaGYxVqC_Y33Mn0Yi9gMQ";
+
 
     async function checkProfile(){
         if(mobileScreen) return;
         if(!profile.username){
             //we want to fetch profile from server
             try {
-                const {data} = await profileApi.fetch(token);
+                const {data} = await profileApi.fetch();
                 console.log("Fetch results", data);
-                dispatch(saveProfileDetails(data));
+                dispatch(saveProfileDetails({data}));
             } catch (error) {
                 console.log("Fetch Error:",error);
             }

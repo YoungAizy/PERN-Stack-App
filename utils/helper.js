756189b16796833b8ae55b74c42109e060936b0d
youngAizy <ayandamarotya@gmail.com>
2023-12-13 22:48:41 +0200

    set expires for cookie and make expiration a Date obj

--- utils/helper.js
+++ utils/helper.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
+
 const unixHour = 60000 * 60;
 const dayUnix = unixHour * 24;
-const expirationTime = Date.now() + dayUnix;
+const expirationTime = new Date(Date.now() + dayUnix);
 
 exports.sendTokens = (tokens, res)=>{
     const {IdToken, ...accessTokens} = tokens;
     const decoded = jwt.decode(IdToken);
-    const isVerified = decoded.email_verified
-    res.cookie("idToken". IdToken, {httpOnly: true, sameSite: "lax", maxAge: expirationTime});
+    const isVerified = decoded.email_verified;
+    res.cookie("idToken", IdToken, {httpOnly: true, sameSite: "lax", expires: expirationTime});
     res.json({accessTokens,isVerified});
 }

1efdc786abe44dff8a62da10ecb3bf3f9ed0e38c
youngAizy <ayandamarotya@gmail.com>
2023-12-14 10:49:52 +0200

    add disable linting on useEffect

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -56,6 +56,7 @@ export default function DashboardPage(){
         }
     }
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     useEffect(()=> checkProfile() ,[])
 
     if (mobileScreen) {

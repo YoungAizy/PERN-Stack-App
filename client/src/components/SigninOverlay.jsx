afa4eb233d854c64ea3a5d1c94f6a35b06dea46f
youngAizy <ayandamarotya@gmail.com>
2023-12-14 10:49:02 +0200

    make overlay position to be fixed

--- client/src/components/SigninOverlay.jsx
+++ client/src/components/SigninOverlay.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SigninOverlay = () => {
   return (
-    <div className='position-absolute z-3 top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center' 
+    <div className='position-fixed z-3 top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center' 
         style={{backgroundColor:"#f9f1e7f2"}}>
         <div className=''>
             <h2>Signing You In ...</h2>

5c217fcd216c5a5a5cb73c2b5429db54cd249022
youngAizy <ayandamarotya@gmail.com>
2023-12-15 09:23:35 +0200

    add test routes responses

--- main.js
+++ main.js
@@ -18,15 +18,17 @@ app.use(hpp());
 app.use(express.static("./public"))
 
 app.use('/api/v1/auth',AuthRouter);
+
 app.post('/api/v1/auth/test',(req,res)=>{
     console.log(req.headers.authorization);
+    res.send("Test 1 hit")
 })
 
-app.get('/user', (req,res)=>{
-    var pem = fs.readFileSync('./private-key.pem',"utf8");
-    const user = {name: "ayanda"}
-    const jws = jwt.sign(user,pem,{algorithm:'RS256'});
-    res.send(jws);
+app.get('/test2', (req,res)=>{
+    // var pem = fs.readFileSync('./private-key.pem',"utf8");
+    // const user = {name: "ayanda"}
+    // const jws = jwt.sign(user,pem,{algorithm:'RS256'});
+    res.send("Test 2 Hitt.");
 });
 
 const port = process.env.PORT

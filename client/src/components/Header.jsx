7f676ebbdb164c6b094071782f6dcce9e20fe268
youngAizy <ayandamarotya@gmail.com>
2023-12-15 08:59:49 +0200

    show logged in buttons on header only for reviewers

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,14 +12,14 @@ const Header = ({ setShow }) => {
 
     const history = useHistory();
     const isAuthenticated = localStorage.getItem("isAuthenticated");
-    // const isAuthenticated = true;
+    const userType = localStorage.getItem("user_type");
 
     return (
         <header className="home-header position-relative" style={{ backgroundImage: `url(${Background})` }}>
             <h1 className="font-weight-light display-1 text-center" style={{ color: "#ef8a64" }}>Restaurant Reviewer</h1>
 
             <>
-                {isAuthenticated ? <LoggedInBtns history={history} />
+                {isAuthenticated && userType === "reviewer" ? <LoggedInBtns history={history} />
                     : <SignInButtons setShow={setShow} history={history} />}
             </>
         </header>

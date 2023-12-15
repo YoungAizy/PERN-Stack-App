877297ea37f86919cb6873f260be4ccc8c571260
youngAizy <ayandamarotya@gmail.com>
2023-12-15 08:50:46 +0200

    check reviewer profile if it is fetched from server

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import NotificationsPage from '../components/dashboard/reviewer/NotificationsPage';
 import ReviewsPage from '../components/dashboard/reviewer/ReviewsPage';
 import ProfilePage from '../components/dashboard/reviewer/ProfilePage';
+import { useDispatch, useSelector } from 'react-redux';
+import { saveProfileDetails } from '../store/actions/profileActions';
+import { saveUser } from '../store/actions/userActions';
+import profileApi from '../apis/profile';
 
 const Home = () => {
     const history = useHistory();
     const {page} = useParams();
+
+    const profile = useSelector(state=> state.profile.profile);
+    const dispatch = useDispatch();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(()=> checkProfile() ,[])
+
+    async function checkProfile(){
+        if(!profile.username){
+            //we want to fetch profile from server
+            try {
+                const {data} = await profileApi.fetch();
+                console.log("Fetch results", data);
+                dispatch(saveProfileDetails({data:data.profile}));
+                dispatch(saveUser({data:data.user}));
+            } catch (error) {
+                console.log("Fetch Error:",error);
+            }
+        }
+    }
 
 
     const checkPage = ()=>{

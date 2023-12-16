1d65f6c3d8db8c6e3fd35eaefe8c4d3161e650a1
youngAizy <ayandamarotya@gmail.com>
2023-12-16 20:53:31 +0200

    add restaurants schema

--- client/src/utils/requestObjects.js
+++ client/src/utils/requestObjects.js
@@ -5,6 +5,8 @@ export const updateProfile = (username,city,d_o_b,gender)=> ({username,city,d_o_
 export const profileSchema = (username,gender,d_o_b,city,
     user_type,companyName,companyPosition) => ({username,gender,d_o_b,city,user_type,companyName,companyPosition});
 
-export const restaurantSchema = () => ({});
+export const restaurantSchema = (name, addr_, price, description, createdby, tel, tel_ext, email, web, city) => {
+    return {name, addr_, price, description, createdby, tel, tel_ext, email, web, city}
+};
 
 export const newReview = () => ({})

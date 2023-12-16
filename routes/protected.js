b1a5638d326e8444f4e28a6a29430ab03a106141
youngAizy <ayandamarotya@gmail.com>
2023-12-16 13:20:20 +0200

    configure multer

--- routes/protected.js
+++ routes/protected.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import { addRestaurant, update, del_Restaurant, upVote, downVote } from '../controllers/restaurantController.js';
 import { new_review, del_review, new_like, dislike, unlike, remove_disLike } from '../controllers/reviewsController.js';
+import multer from 'multer';
+
 export const restaurant_Router = express.Router();
 export const reviewsRouter = express.Router();
 
+// configure multer
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage });
+
 //RESTAURANT ROUTES
-restaurant_Router.post("/add", addRestaurant);
+restaurant_Router.post("/add", upload.single('image'), addRestaurant);
 restaurant_Router.put("/update/:id", update);
 restaurant_Router.patch("/upvote/:id",upVote);
 restaurant_Router.patch("/downvote/:id",downVote);

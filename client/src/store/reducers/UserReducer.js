99c9e8e3466e4035bd872015abed529fd90f478f
youngAizy <ayandamarotya@gmail.com>
2023-12-23 00:26:00 +0200

    update userReducer

--- client/src/store/reducers/UserReducer.js
+++ client/src/store/reducers/UserReducer.js
@@ -1,16 +1,27 @@
 import { USER_TYPES } from "../actionTypes";
 
 const defaultState = {
-    verificationEmail: "",
-    user: {}
+    verification : {
+        email: "",
+        password: ""
+    },
+    user: {},
+    tokens:{
+        accessToken: (localStorage.getItem("tokens") && JSON.parse(localStorage.getItem("tokens"))["AccessToken"]) || null,
+        refreshToken: null,
+    }
 }
 
 const UserReducer = (state = defaultState, action)=>{
     switch (action.type) {
-        case USER_TYPES.STORE_VERIFICATION_EMAIL:
-            return {...state, verificationEmail: action.payload.email};
+        case USER_TYPES.STORE_VERIFICATION_DETAILS:
+            return {...state, verification: action.payload.data};
+        case USER_TYPES.DELETE_VERIFICATION_DETAILS:
+            return {...state, verification: {}};
         case USER_TYPES.SAVE_USER_DETAILS:
-            return {...state, user: action.payload.data}
+            return {...state, user: action.payload.data};
+        case USER_TYPES.SAVE_TOKENS:
+            return {...state, tokens: action.payload.data};
         default:
             return state;
     }

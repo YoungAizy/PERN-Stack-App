096f5d54f878436e79c8ff65798d973f9a2721d0
youngAizy <ayandamarotya@gmail.com>
2023-12-23 01:18:32 +0200

    update verifyEmail component

--- client/src/components/registration/VerifyEmail.jsx
+++ client/src/components/registration/VerifyEmail.jsx
@@ -4,22 +4,30 @@ import Button from '../styled/Button';
 import requestBody from '../../utils/requestBody';
 import { userRequests } from '../../utils/requestTypes';
 import authApi from '../../apis/auth';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+import useQuery from '../../hooks/useQuery';
+import useCheckType from '../../hooks/useCheckType';
+import { deleteVerification } from '../../store/actions/userActions';
+import { saveAcessTokens } from '../../utils/getAccessToken';
 
 const VerifyEmail = ({onPageChange, setBackgroundHeight, dispatch}) => {
-    
-    const email = useSelector(state=> state.user.verificationEmail);
+    const reduxDispatch = useDispatch()
+    const verificationParams = useSelector(state=> state.user.verification);
+    const email = verificationParams?.email;
+    const password = verificationParams?.password;
     const [verificationCode, setVerificationCode] = useState("");
     const [invalid, setInvalid] = useState(false);
     const history = useHistory();
+    const query = useQuery();
+    const checkUserType = useCheckType();
 
     // if the email to be verified is not stored in redux, 
     // reroute user to login page to ptompt for an email and reroute them back for verification
     if(!email){
         history.push('/signin');
     }
-    
+
     const getNewCode = async e=>{
         e.preventDefault();
         try {
@@ -30,6 +38,33 @@ const VerifyEmail = ({onPageChange, setBackgroundHeight, dispatch}) => {
             alert("Couldn't resend code");
         }
     }
+
+    const cleanup = ()=>{
+        reduxDispatch(deleteVerification());
+    }
+
+    const verifyEmail = async()=>{
+        try {
+            const body = requestBody(userRequests.updateEmailVerification, {email, confirmationCode: verificationCode, password, attr: "email" })
+            const {data} = await authApi.verifyEmailChange(body);
+            console.log("verified",data)
+            if(data.accessTokens){
+                cleanup();
+                saveAcessTokens(data.accessTokens);
+                checkUserType(localStorage.getItem("user_type"));
+            }
+            
+        } catch (error) {
+            console.log("VERIFY EMAIL ERR:",error);
+        }
+    }
+
+    const updatePage = ()=>{
+        dispatch({type:"Done", step: "step2"});
+        dispatch({type:"Active", step: "step3"});
+        onPageChange(3);
+        setBackgroundHeight('fit-content');
+    }
     
     const btnClick = async e =>{
         e.preventDefault();
@@ -39,10 +74,15 @@ const VerifyEmail = ({onPageChange, setBackgroundHeight, dispatch}) => {
             return;
         }
 
+        
+        if(query.get("verification_type") === "email_update"){
+            verifyEmail();
+            return;
+        }
+
         const body = requestBody(userRequests.VERIFICATION,{email, verificationCode});
         try {
             const {data, status} = await authApi.verifyUser(body);
-            console.log("V")
             if(data.ErrorMessage && status === 203){
                 console.log(data.ErrorMessage);
                 alert(data.ErrorMessage);
@@ -50,12 +90,15 @@ const VerifyEmail = ({onPageChange, setBackgroundHeight, dispatch}) => {
             }
             if(status === 200 && data.accessTokens){
                 localStorage.setItem("tokens", JSON.stringify(data.accessTokens));
+                if(query.get('type') && (query.get('type') === "update")){
+                    const userType = localStorage.getItem('user_type');
+                    checkUserType(userType);
+                    return;
+                }
                 localStorage.setItem("isAuthenticated", true);
                 console.log("Verification results:",data);// check result.status==200 and result.statusText == "OK"
-                dispatch({type:"Done", step: "step2"})
-                dispatch({type:"Active", step: "step3"})
-                onPageChange(3);
-                setBackgroundHeight('fit-content');
+                cleanup();
+                updatePage();
             }
         } catch (error) {
             console.log("Verification Failed:", error);

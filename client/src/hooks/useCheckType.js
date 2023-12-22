ab378b3403226cd9843fe771ed5f22b5986970b1
youngAizy <ayandamarotya@gmail.com>
2023-12-23 00:32:34 +0200

    create a cutom hook to reroute user to their pages

---
+++ client/src/hooks/useCheckType.js
@@ -0,0 +1,26 @@
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
+
+
+const useCheckType = ()=>{
+    const history = useHistory();
+
+    function push(userType){
+        switch (userType) {
+            case "reviewer":
+                //window.location.pathname = "/manage";
+                history.push('/home/notifications')
+                break;
+        
+            case "restaurateur":
+                history.push('/dashboard/manage');
+                break;
+            default:
+                history.push("/signin");
+                break;
+        }
+    }
+
+    return push;
+}
+
+export default useCheckType;

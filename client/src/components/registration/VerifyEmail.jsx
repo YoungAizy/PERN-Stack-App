fcb2fb2415700ebb52c63e557c43d9b5679dd13d
youngAizy <ayandamarotya@gmail.com>
2023-12-23 14:15:58 +0200

    move  redux cleanup function to a useEffect

--- client/src/components/registration/VerifyEmail.jsx
+++ client/src/components/registration/VerifyEmail.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import FloatingInputField from '../styled/FloatingInput';
 import Button from '../styled/Button';
 import requestBody from '../../utils/requestBody';
@@ -9,15 +9,20 @@ import { useHistory } from 'react-router-dom/cjs/react-router-dom';
 import useQuery from '../../hooks/useQuery';
 import useCheckType from '../../hooks/useCheckType';
 import { deleteVerification } from '../../store/actions/userActions';
-import { saveAcessTokens } from '../../utils/getAccessToken';
+import { saveAcessTokens, saveAuth } from '../../utils/getAccessToken';
 
 const VerifyEmail = ({onPageChange, setBackgroundHeight, dispatch}) => {
     const reduxDispatch = useDispatch()
     const verificationParams = useSelector(state=> state.user.verification);
     const email = verificationParams?.email;
     const password = verificationParams?.password;
+
     const [verificationCode, setVerificationCode] = useState("");
     const [invalid, setInvalid] = useState(false);
+    const [onTransmission, setOnTransmission] = useState(false);
+    const [seconds, setSeconds] = useState(60);
+    const [resendDisabled, setResendDisabled] = useState(true);
+
     const history = useHistory();
     const query = useQuery();
     const checkUserType = useCheckType();
@@ -28,10 +33,29 @@ const VerifyEmail = ({onPageChange, setBackgroundHeight, dispatch}) => {
         history.push('/signin');
     }
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(()=>cleanup,[])
+
+    useEffect(()=>{
+        if(seconds <= 0){
+            setResendDisabled(false);
+            return;
+        }
+
+        const resendTimeOut = setInterval(()=>{ 
+            setSeconds( prevSecs => prevSecs-1)
+        }, 1000);
+
+        return ()=> clearInterval(resendTimeOut);
+    },[seconds])
+
     const getNewCode = async e=>{
         e.preventDefault();
+        setResendDisabled(true);
+        setSeconds(60);
         try {
             const {data} = await authApi.resendVerificationCode(email);
+            console.log(data);
             data.verified && alert(`Check your inbox`);
         } catch (error) {
             console.log("Error resending verification code", error);
@@ -44,6 +68,7 @@ const VerifyEmail = ({onPageChange, setBackgroundHeight, dispatch}) => {
     }
 
     const verifyEmail = async()=>{
+        setOnTransmission(true);
         try {
             const body = requestBody(userRequests.updateEmailVerification, {email, confirmationCode: verificationCode, password, attr: "email" })
             const {data} = await authApi.verifyEmailChange(body);
@@ -60,21 +85,22 @@ const VerifyEmail = ({onPageChange, setBackgroundHeight, dispatch}) => {
     }
 
     const updatePage = ()=>{
+        setBackgroundHeight('fit-content');
         dispatch({type:"Done", step: "step2"});
         dispatch({type:"Active", step: "step3"});
         onPageChange(3);
-        setBackgroundHeight('fit-content');
     }
     
     const btnClick = async e =>{
         e.preventDefault();
-
+   
+        // updatePage();
         if(verificationCode.length < 4){
             setInvalid(true);
             return;
         }
 
-        
+        setResendDisabled(true);
         if(query.get("verification_type") === "email_update"){
             verifyEmail();
             return;
@@ -82,28 +108,31 @@ const VerifyEmail = ({onPageChange, setBackgroundHeight, dispatch}) => {
 
         const body = requestBody(userRequests.VERIFICATION,{email, verificationCode});
         try {
+            setOnTransmission(true);
             const {data, status} = await authApi.verifyUser(body);
             if(data.ErrorMessage && status === 203){
+                setOnTransmission(false);
                 console.log(data.ErrorMessage);
                 alert(data.ErrorMessage);
                 return;
             }
             if(status === 200 && data.accessTokens){
-                localStorage.setItem("tokens", JSON.stringify(data.accessTokens));
+
                 if(query.get('type') && (query.get('type') === "update")){
                     const userType = localStorage.getItem('user_type');
                     checkUserType(userType);
                     return;
                 }
-                localStorage.setItem("isAuthenticated", true);
+                saveAuth(data.accessTokens)
                 console.log("Verification results:",data);// check result.status==200 and result.statusText == "OK"
-                cleanup();
-                updatePage();
+                
             }
         } catch (error) {
+            setOnTransmission(false);
             console.log("Verification Failed:", error);
             return;
         }
+        updatePage();
       }
 
   return (
@@ -112,9 +141,12 @@ const VerifyEmail = ({onPageChange, setBackgroundHeight, dispatch}) => {
             <p className='form-margin'>Enter the code sent to your Email below.</p>
             <FloatingInputField value={verificationCode} inputId={"signup_verification_code"} inputType={"text"} 
                 label={"Verification Code"} placeholder={"Verification code"} onInputChanged={setVerificationCode}  />
-            <button className='btn btn-secondary form-margin mt-1' onClick={getNewCode}>Resend</button>
+            <div>
+                <button className='btn btn-secondary form-margin mt-1' onClick={getNewCode} disabled={resendDisabled}>Resend</button>
+                <span style={{color: "white", paddingBottom:"1.2rem"}}>{seconds}</span>
+            </div>
             {invalid && <p className='form-margin ps-2' style={{backgroundColor:"crimson", color:"ghostwhite"}}>Invalid Code length. Try Again.</p>}
-            <Button text={"Next"} btnType={"submit"} placement={"flex-end"} onBtnClick={btnClick}/>
+            <Button text={"Next"} btnType={"submit"} placement={"flex-end"} onBtnClick={btnClick} disabled={onTransmission}/>
         </form>
     </div>
   )

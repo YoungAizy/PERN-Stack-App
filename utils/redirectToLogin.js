8a15010a81673a2cb12498007bb43dbe46d478a0
youngAizy <ayandamarotya@gmail.com>
2023-12-23 15:27:32 +0200

    update login redirect file

--- utils/redirectToLogin.js
+++ utils/redirectToLogin.js
@@ -1,12 +1,24 @@
 const {unverifiedUsers} = require('../memory/signup_dictionary.js');
 const { Authenticate} = require('../services/auth.service.js');
 
-exports.redirectToLoginService = async (email)=>{
+exports.redirectToLoginService = async (res, email, password)=>{
+    console.log("dictionary: ", unverifiedUsers);
+    console.log("email: ", email);
+    console.log(unverifiedUsers[email]);
     //first lookup password in signup dictionary;
-    const userPassword = unverifiedUsers[email];
+    const userPassword = unverifiedUsers[email] || password;
+    console.log("password:", userPassword);
 
-    const tokens = await Authenticate("USER_PASSWORD_AUTH", {"USERNAME":email,"PASSWORD":userPassword});
-    tokens && delete unverifiedUsers[email];
+    const unixHour = 60000 * 60;
+    const dayUnix = unixHour * 24;
+    const expirationTime = Date.now() + dayUnix;
 
-    return tokens;
+    const authTokens = await Authenticate("USER_PASSWORD_AUTH", {"USERNAME":email,"PASSWORD":userPassword});
+    authTokens && delete unverifiedUsers[email];
+
+    const {IdToken, ... accessTokens} = authTokens;
+    IdToken && res.cookie("idToken", IdToken,{httpOnly: true, sameSite: "lax", maxAge: expirationTime});
+    console.log("User Verified:", accessTokens);
+
+    return accessTokens;
 }

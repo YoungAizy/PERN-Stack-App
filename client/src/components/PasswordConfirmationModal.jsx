a203e7d0cabe07c1004af14f41335d17ce386ce6
youngAizy <ayandamarotya@gmail.com>
2023-12-23 08:52:33 +0200

    add submit function to pass word confirmation component

--- client/src/components/PasswordConfirmationModal.jsx
+++ client/src/components/PasswordConfirmationModal.jsx
@@ -2,11 +2,16 @@ import React, { useState } from 'react'
 import Modal from './styled/Modal';
 import { HintedLabelInput } from './styled/TextInput';
 
-const PasswordConfirmationModal = ({closeModal}) => {
+const PasswordConfirmationModal = ({closeModal, submit}) => {
     const [password,setPassword] = useState("");
 
+    const _submit = ()=>{
+      console.log("Hello", password);
+      submit(password);
+    }
+
   return (
-    <Modal title={"Confirm Password"} closeModal={closeModal} submitBtnText={"Confirm"} ruler={true}>
+    <Modal title={"Confirm Password"} closeModal={closeModal} onSubmitClick={_submit} submitBtnText={"Confirm"} ruler={true}>
         <HintedLabelInput val={password} inputType={"password"} inputId={"password_check"} label={"Password"} onChangeEvent={setPassword} />
     </Modal>
   )

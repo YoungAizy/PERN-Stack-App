301df7ba875026779ce42423de912caaf5adaf6d
youngAizy <ayandamarotya@gmail.com>
2023-12-23 15:34:40 +0200

    move unix timestamp inside of function and check IdToken before setting cookie

--- utils/helper.js
+++ utils/helper.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken');
 
-const unixHour = 60000 * 60;
-const dayUnix = unixHour * 24;
-const expirationTime = new Date(Date.now() + dayUnix);
-
 exports.sendTokens = (tokens, res)=>{
+    const unixHour = 60000 * 60;
+    const dayUnix = unixHour * 24;
+    const expirationTime = new Date(Date.now() + dayUnix);
+
     const {IdToken, ...accessTokens} = tokens;
     const decoded = jwt.decode(IdToken);
     const isVerified = decoded.email_verified;
-    res.cookie("idToken", IdToken, {httpOnly: true, sameSite: "lax", expires: expirationTime});
+    IdToken && res.cookie("idToken", IdToken, {httpOnly: true, sameSite: "lax", expires: expirationTime});
     res.json({accessTokens,isVerified});
 }

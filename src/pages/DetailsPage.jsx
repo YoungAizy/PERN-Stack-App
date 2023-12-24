895c305451d29c1e6f7253272d5724d32516bf09
youngAizy <ayandamarotya@gmail.com>
2023-12-24 04:00:54 +0200

    use useQuery to fetch data

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import AddReview from '../components/AddReview';
@@ -9,28 +9,29 @@ import { addSelectedData } from '../store/actions/restaurantActions';
 import { saveSelectedRestaurantReviews } from '../store/actions/reviewsActions';
 import Default from "../assets/default.jpg";
 import { _public } from '../apis/restaurants';
+import { useQuery } from 'react-query';
 
 
-const DetailsHeader = (props) => {
+const DetailsHeader = ({restaurant, street, city, image, rating, count}) => {
     return (
         <div >
             <header id='details-header' className='d-flex justify-content-between'>
                 <div className='detail-img col-2 rounded'>
-                    <img src={props.image ? props.image : Default} alt="" />
+                    <img src={ image ? image : Default} alt="" />
                 </div>
                 <div className='col-8' style={{ paddingLeft: ".6rem", paddingRight: ".6rem" }}>
                     <div>  
                         <h5 className="display-5">
-                            {props.restaurant}
+                            {restaurant}
                         </h5>
-                        <StarRating rating={props.rating} />
-                        <span className="text-warning">{props.count ? `(${props.count})` : "(0)"}</span>
+                        <StarRating rating={rating} />
+                        <span className="text-warning">{count ? `(${count})` : "(0)"}</span>
                     </div>
-                    <div> {props.street}<br />{props.suburb}</div>
+                    <div className='mt-2'> {street}<br />{city}</div>
                 </div>
                 <div className='col-1' style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
                     <Link to="/">
-                        <i className="fas fa-home fa-lg" style={{color:'white',fontSize:'xx-large'}}></i>
+                        <i className="fas fa-home fa-lg" style={{color:'coral',fontSize:'xx-large'}}></i>
                     </Link>
                 </div>
             </header>
@@ -48,24 +49,28 @@ const DetailsPage = () => {
     console.log("selected reviews", reviews)
 
     const fetchData = async () => {
-        const { data } = await _public.singlePartial(id);
-        console.log(data)
+        let data;
+        if(selected.name){
+            console.log("K", selected);
+            ({ data } = await _public.singlePartial(id))
+        }else{
+            console.log("HPJ");
+            ({ data } = await _public.singleAll(id))
+        }
+        console.log("Returned data:", data)
         dispatch(addSelectedData({data:data.restaurant}));
         dispatch(saveSelectedRestaurantReviews({data:data.reviews}));
         setImgSrc(data.restaurant.img_url);
 
     }
-    useEffect(() => {
-        fetchData();
-        // eslint-disable-next-line 
-    }, []);
+    useQuery('restaurant_page',fetchData);
 
     return (
         <div>
 
             {selected && (
                 <>
-                    <DetailsHeader image={imgsrc && imgsrc} restaurant={selected.name} street={selected["_str/sub"]} suburb={selected.city}
+                    <DetailsHeader image={imgsrc && imgsrc} restaurant={selected.name} street={selected["str/sub"]} city={selected.city}
                         rating={selected.avg_rating} count={selected.total_reviews} />
                     <div className="d-flex">
 
@@ -76,7 +81,7 @@ const DetailsPage = () => {
                                 {selected.description ? <p>{selected.description}</p> : <h3>No Description given</h3>}
                             </div>
                         </div>
-                        <div className="mt-3 mb-4">
+                        <div className="mt-4 mb-4">
                             <div >
                                 <Reviews reviews={reviews} />
                             </div>
@@ -89,9 +94,9 @@ const DetailsPage = () => {
                             <h5>Contact Information</h5>
                             <hr/>
                             <p><strong>Address: </strong> </p>
-                            {selected.email_address && <p><strong>E-mail: </strong> <a href={`mailto:${selected.email_address}`}>{selected.email_address}</a></p>}
-                            {selected._telephone && <p><strong>Telephone: </strong> <a href={`tel:${selected._telephone}`}>{selected._telephone}</a> </p>}
-                            {selected.website && <p><strong>Web: </strong><a href={selected.website} target="_blank" rel="noopener noreferrer">{selected.website}</a> </p>}
+                            <p><strong>E-mail: </strong>{selected.email_addr ? <a href={`mailto:${selected.email_addr}`}>{selected.email_addr}</a>: " Not Supplied" }</p>
+                            <p><strong>Telephone: </strong>{selected.telephone ? <a href={`tel:${selected.telephone}`}>{selected.telephone}</a>: " Not Supplied" }</p>
+                            <p><strong>Web: </strong>{ <a href={selected.web_addr} target="_blank" rel="noopener noreferrer">{selected.web_addr}</a> } </p>
                             </div>
                         </div>
                     </div>

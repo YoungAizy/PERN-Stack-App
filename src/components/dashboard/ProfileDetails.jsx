c81b6aadb7c1b15f6490e13c760ce99c53c04e7f
youngAizy <ayandamarotya@gmail.com>
2024-01-18 16:06:41 +0200

    pass state updating functions directly to input components

--- src/components/dashboard/ProfileDetails.jsx
+++ src/components/dashboard/ProfileDetails.jsx
@@ -20,20 +20,20 @@ const ProfileDetails = ({ isReviewer = false, inputClasses,dobClasses, genderCla
     const [gender,setGender] = useState(profile.gender);
     const [dob,setDob] = useState(profile.d_o_b);
 
-    const trackChanges = (type)=>{
-      switch (type) {
-        case 'username':
-          return setUsername;
-        case 'city':
-          return setCity;
-        case 'gender':
-          return setGender;
-        case 'birthday':
-          return setDob;
-        default:
-          return;
-      }
-    }
+    // const trackChanges = (type)=>{
+    //   switch (type) {
+    //     case 'username':
+    //       return setUsername;
+    //     case 'city':
+    //       return setCity;
+    //     case 'gender':
+    //       return setGender;
+    //     case 'birthday':
+    //       return setDob;
+    //     default:
+    //       return;
+    //   }
+    // }
 
     const onUpdateUser = async e=>{
       e.preventDefault();
@@ -53,7 +53,7 @@ const ProfileDetails = ({ isReviewer = false, inputClasses,dobClasses, genderCla
         console.log(data);
         dispatch(saveProfileDetails({data: data.data}));
       } catch (error) {
-        console.log(error);
+        console.log(error.message);
       }
     }
     
@@ -62,8 +62,8 @@ const ProfileDetails = ({ isReviewer = false, inputClasses,dobClasses, genderCla
         {!isReviewer && <h4>Profile:</h4>}
         <form action="" method="post">
           <Avatar initials={"AMD"} bg_color={"orange"} />
-          <TextInput label="Username" inputId="username" val={username} inputType="text" onChangeEvent={trackChanges("username")} fieldName="user_name" additionalClasses={inputClasses} />
-          <TextInput label={"City"} inputId={"city"} val={city} inputType={"text"} fieldName={"city"} onChangeEvent={trackChanges("city")} additionalClasses={inputClasses} />
+          <TextInput label="Username" inputId="username" val={username} inputType="text" onChangeEvent={setUsername} fieldName="user_name" additionalClasses={inputClasses} />
+          <TextInput label={"City"} inputId={"city"} val={city} inputType={"text"} fieldName={"city"} onChangeEvent={setCity} additionalClasses={inputClasses} />
           <GenderOptions onGenderChange={setGender} val={gender} optionalClasses={genderClasses} />
           <DOB setDOB={setDob} dob={dob} parentStyling={dobClasses && dobClasses.parent} 
             yearCol={dobClasses && dobClasses.year} monthCol={dobClasses && dobClasses.month} dayCol={dobClasses && dobClasses.day} />

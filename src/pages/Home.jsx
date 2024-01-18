3bd4974bccf09135e2f5963585339a6396378769
youngAizy <ayandamarotya@gmail.com>
2024-01-18 16:33:15 +0200

    refractor Profile checker to hook + use redux to update active page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,47 +1,31 @@
 import React, { useEffect } from 'react';
-import Header from '../components/Header';
+import Header from '../components/home-header/Header';
 import { useHistory, useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import NotificationsPage from '../components/dashboard/reviewer/NotificationsPage';
 import ReviewsPage from '../components/dashboard/reviewer/ReviewsPage';
 import ProfilePage from '../components/dashboard/reviewer/ProfilePage';
-import { useDispatch, useSelector } from 'react-redux';
-import { saveProfileDetails } from '../store/actions/profileActions';
-import { saveUser } from '../store/actions/userActions';
-import profileApi from '../apis/profile';
+import { useDispatch } from 'react-redux';
 import { Redirect} from 'react-router-dom'
 import userTypes from '../utils/UserTypes';
+import { setActiveDashboardPage } from '../store/actions/reviewerDashboardActions';
 
 const Home = () => {
     const userType = localStorage.getItem("user_type");
     const history = useHistory();
     const {page} = useParams();
 
-    
-    const profile = useSelector(state=> state.profile.profile);
     const dispatch = useDispatch();
     
+    useEffect(()=>{
+        dispatch(setActiveDashboardPage(page));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(()=> checkProfile() ,[]);
+    },[page])
     
     if( userType !== userTypes.reviewer){
         return(
             <Redirect to="/"/>
         )
     }
-    async function checkProfile(){
-        if(!profile.username){
-            //we want to fetch profile from server
-            try {
-                const {data} = await profileApi.fetch();
-                console.log("Fetch results", data);
-                dispatch(saveProfileDetails({data:data.profile}));
-                dispatch(saveUser({data:data.user}));
-            } catch (error) {
-                console.log("Fetch Error:",error);
-            }
-        }
-    }
-
 
     const checkPage = ()=>{
         switch (page) {

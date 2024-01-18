4a469c72834ed3244cd65503ac3cb9b1d495a260
youngAizy <ayandamarotya@gmail.com>
2024-01-18 15:53:24 +0200

    update mockData to reflect new review schema

--- src/components/dashboard/reviewer/ReviewsPage.jsx
+++ src/components/dashboard/reviewer/ReviewsPage.jsx
@@ -14,21 +14,21 @@ const mockData= [
   {
     id: 1,
     rating: 1,
-    review_text: "Just another chicken franchise that wants to make you unhealthy by feeding you poison. Might as well inject me with diabetes directly",
+    review: "Just another chicken franchise that wants to make you unhealthy by feeding you poison. Might as well inject me with diabetes directly",
     username: "I_am_a_troll",
 
   },
   {
     id: 2,
     rating: 4,
-    review_text: "Good food. I especially loved the chicken nuggets",
+    review: "Good food. I especially loved the chicken nuggets",
     username: "Jessica",
 
   },
   {
     id: 3,
     rating: 4,
-    review_text: "Had a lovely first date with my wife here, when the restaurants had just opened 6 years ago. Took her to dinner for our anniversary, and I must compliment the service and the food is still delicious. Had a good time.",
+    review: "Had a lovely first date with my wife here, when the restaurants had just opened 6 years ago. Took her to dinner for our anniversary, and I must compliment the service and the food is still delicious. Had a good time.",
     username: "Paul",
 
   },
